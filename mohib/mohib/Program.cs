05373772fbd20eb83d54e6235fb26a8f79e06028
mohibUllah07 <122685562+mohibUllah07@users.noreply.github.com>
2024-03-11 18:37:45 +0500

    Add files via upload

---
+++ mohib/mohib/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+//Game pubg = new Game("pubg mobile" ,"karaftone" , 9.6);
+//Console.WriteLine($"Game Name:{pubg.GameName} CompanyName: {pubg.GameCompany} GameVersion: {pubg.GameVersion} ");
+
+
+//Console.ReadKey();
+
+//class Game(string GameName , string GameCompany , double GameVersion)
+//{
+//    public string GameName =  GameName; public string GameCompany = GameCompany; public double GameVersion = GameVersion;
+
+//}
+
+
+
+Console.ReadKey();
+
+class product(string name , string de)
+{
+
+}
